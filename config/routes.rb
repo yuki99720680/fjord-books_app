02dd938998874e545326c307be1d71bd397008d2
m-kimura <mk.alstroemeria@gmail.com>
2022-04-13 17:04:07 +0900

    [mod] userのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
   root to: 'books#index'
 
+  resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
