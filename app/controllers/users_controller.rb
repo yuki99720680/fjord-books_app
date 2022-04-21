b41008f76d5082ac5495eb5ce54c086bf1074f17
m-kimura <mk.alstroemeria@gmail.com>
2022-04-21 14:59:03 +0900

    [mod] 詳細と更新の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.order(:id).page(params[:page]).per(1)
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: '[TEST MASSAGE] User update success.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :postal_code, :address, :bio)
   end
 end
