d3d46589c1af597ff31cda690fa4246e03806f39
m-kimura <mk.alstroemeria@gmail.com>
2022-04-21 15:27:51 +0900

    [mod] 他人のユーザー情報の編集をできないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
 
   def show; end
 
-  def edit; end
+  def edit
+    render_not_found unless current_user == @user
+  end
 
   def update
     if @user.update(user_params)
@@ -27,5 +29,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :postal_code, :address, :bio)
+  end
+
+  def render_not_found
+    respond_to do |format|
+      format.html { render file: 'public/404.html', layout: false, status: :not_found }
+    end
   end
 end
