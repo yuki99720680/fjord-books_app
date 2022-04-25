c1c104c884fbbc1ef2216f232de3115cbfb7040d
m-kimura <mk.alstroemeria@gmail.com>
2022-04-25 16:02:56 +0900

    [clean] rubocop対策

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,12 +20,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
-
-  def after_update_path_for(resource)
-    @user
+  def update
+    super
   end
 
   # DELETE /resource
@@ -52,6 +48,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address bio])
+  end
+
+  def after_update_path_for(user)
+    user
   end
 
   # The path used after sign up.
