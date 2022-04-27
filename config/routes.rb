479d0feccb43a7b14e97fe2c8c67772fbf81c744
m-kimura <mk.alstroemeria@gmail.com>
2022-04-27 13:47:55 +0900

    [mod] g controller Followings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'followings/index'
+  get 'followings/create'
+  get 'followings/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
