267c85869ea6074e451b5e4c37a65a611395477c
m-kimura <mk.alstroemeria@gmail.com>
2022-04-28 15:07:35 +0900

    [mod] フォロー機能実装

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,10 +1,18 @@
 class FollowingsController < ApplicationController
-  def index
-  end
+  before_action :set_user, only: %i[create destroy]
+
+  def index; end
+  # current_user.following
 
   def create
+    redirect_to request.referer, notice: 'フォローしたよ！' if current_user.following.create(follower_id: @user.id)
   end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
