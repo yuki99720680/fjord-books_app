7cb6db499f551550e4dfe17893ebeb54917e22fa
m-kimura <mk.alstroemeria@gmail.com>
2022-05-02 17:12:59 +0900

    [mod] 制約追加

--- db/migrate/20220502074208_create_reports.rb
+++ db/migrate/20220502074208_create_reports.rb
@@ -1,7 +1,7 @@
 class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :body
 
       t.timestamps
