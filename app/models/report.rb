89f9cac9273cf287385374ef62d607585f59f288
m-kimura <mk.alstroemeria@gmail.com>
2022-05-09 15:17:12 +0900

    [mod] 関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,5 +3,7 @@
 class Report < ApplicationRecord
   validates :title, presence: true
   validates :user, presence: true
+
+  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
 end
