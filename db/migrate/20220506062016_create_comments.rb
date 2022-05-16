9ee8de0174a46d137923e8fe7e657040cd92898e
m-kimura <mk.alstroemeria@gmail.com>
2022-05-16 13:34:27 +0900

    [clean] 条件式の変更&関連付けを統合

--- db/migrate/20220506062016_create_comments.rb
+++ db/migrate/20220506062016_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.text :body, null: false
       t.references :commentable, polymorphic: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
