fea040251a732f2fa71bd6fa9a2f14d4abaa4311
m-kimura <mk.alstroemeria@gmail.com>
2022-05-27 14:56:14 +0900

    [clean] rubocop対応

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :github
-  
+
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
