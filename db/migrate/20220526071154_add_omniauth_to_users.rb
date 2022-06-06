1cebdf97b4be38e4b6b0b7e83ea34a724e437525
m-kimura <mk.alstroemeria@gmail.com>
2022-06-06 10:03:08 +0900

    [mod] ユニーク制約を追加

--- db/migrate/20220526071154_add_omniauth_to_users.rb
+++ db/migrate/20220526071154_add_omniauth_to_users.rb
@@ -4,5 +4,7 @@ class AddOmniauthToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
+
+    add_index :users, %i[uid provider], unique: true
   end
 end
