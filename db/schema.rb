10d1a5b6f04502f07b7593783be4573e14d5d3ee
m-kimura <mk.alstroemeria@gmail.com>
2022-06-08 09:27:22 +0900

    [mod] schema.rbにマイグレーションファイルの変更を反映

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_05_26_071154) do
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
